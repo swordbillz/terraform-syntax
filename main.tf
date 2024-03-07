d4284b5f48d30fc01fca24bd6f72fba888a20c33
swordbill <swordbillz@gmali.com>
2024-03-07 11:11:37 +0900

    Create subnet

--- main.tf
+++ main.tf
@@ -13,9 +13,45 @@ provider "aws" {
 }
 
 # Create a VPC
-resource "aws_vpc" "example" {
+resource "aws_vpc" "vpc-01" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "S-VPC-01"
-  }
+    Name = "main"
+  }
+}
+
+# Create a Public Subnet
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id     = aws_vpc.vpc-01.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "hangramit_public_subnet_1"
+  }
+}
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id     = aws_vpc.vpc-01.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "hangramit_public_subnet_2"
+  }
+}
+#Create a Private Subnet
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id     = aws_vpc.vpc-01.id
+  cidr_block = "10.0.110.0/24"
+
+  tags = {
+    Name = "hangramit_private_subnet_1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id     = aws_vpc.vpc-01.id
+  cidr_block = "10.0.111.0/24"
+
+  tags = {
+    Name = "hangramit_private_subnet_2"
+  }
 }
