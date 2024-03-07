a39cce41d12c81e6233a90f8b237ff1ecdc8e608
swordbill <swordbillz@gmali.com>
2024-03-07 14:21:21 +0900

    작업완료

--- main.tf
+++ main.tf
@@ -12,50 +12,12 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-# Create a VPC
-resource "aws_vpc" "vpc-01" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "main"
-  }
+# 개발환경
+module "default_custom_vpc" {
+  source = "./custom_vpc"
 }
 
-# Create a Public Subnet
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id     = aws_vpc.vpc-01.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "hangramit_public_subnet_1"
-  }
-}
-resource "aws_subnet" "public_subnet_2" {
-  vpc_id     = aws_vpc.vpc-01.id
-  cidr_block = "10.0.2.0/24"
-
-  tags = {
-    Name = "hangramit_public_subnet_2"
-  }
-}
-#Create a Private Subnet
-resource "aws_subnet" "private_subnet_1" {
-  vpc_id     = aws_vpc.vpc-01.id
-  cidr_block = "10.0.110.0/24"
-
-  tags = {
-    Name = "hangramit_private_subnet_1"
-  }
-}
-
-resource "aws_subnet" "private_subnet_2" {
-  vpc_id     = aws_vpc.vpc-01.id
-  cidr_block = "10.0.111.0/24"
-
-  tags = {
-    Name = "hangramit_private_subnet_2"
-  }
-}
-
-data "aws_subnet" "private_subnet_1" {
-  id = "subnet-0b24bd0acd8199fb5"
+# 운영환경
+module "prd_custom_vpc" {
+  source = "./custom_vpc"
 }
